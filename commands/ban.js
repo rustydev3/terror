7df4dbaf5fdae3a932b5f5e6bd32d4ab5d051045
rustydev3 <157342958+rustydev3@users.noreply.github.com>
2024-09-21 21:09:15 +0530

    Update ban.js

--- commands/ban.js
+++ commands/ban.js
@@ -1,22 +1,15 @@
-const { PermissionsBitField } = require('discord.js');
-
 module.exports = {
     name: 'ban',
-    description: 'Ban a member from the server.',
-    async execute(message, args) {
-        if (!message.member.permissions.has(PermissionsBitField.Flags.BanMembers)) {
-            return message.reply("You don't have permission to ban members.");
-        }
-
+    description: 'Ban a member from the server',
+    run: async (client, message, args) => {
         const member = message.mentions.members.first();
         if (!member) return message.reply('Please mention a member to ban.');
 
-        try {
-            await member.ban();
-            message.reply(`Banned ${member.user.tag} from the server.`);
-        } catch (err) {
-            console.error(err);
-            message.reply("I couldn't ban that member.");
+        if (!message.member.permissions.has('BAN_MEMBERS')) {
+            return message.reply("You don't have permission to use this command.");
         }
+
+        await member.ban();
+        message.reply(`${member.user.tag} has been banned.`);
     },
 };
