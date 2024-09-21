36f411827b557b9f04b70dbd3917dfc447433048
rustydev3 <157342958+rustydev3@users.noreply.github.com>
2024-09-21 21:10:12 +0530

    Update warns.js

--- commands/warns.js
+++ commands/warns.js
@@ -1,18 +1,21 @@
-const warns = require('../warns.json');
+const fs = require('fs');
+const warnsPath = '../warns.json';
 
 module.exports = {
     name: 'warns',
-    description: 'Show warnings for a mentioned member.',
-    execute(message) {
+    description: 'Show warns of the mentioned member',
+    run: async (client, message, args) => {
         const member = message.mentions.members.first();
+        
         if (!member) return message.reply('Please mention a member to check warnings.');
 
-        const guildWarns = warns[message.guild.id];
-        if (guildWarns && guildWarns[member.id]) {
-            const memberWarns = guildWarns[member.id].warns;
-            message.reply(`${member.user.tag} has the following warnings: ${memberWarns.join(', ')}`);
-        } else {
-            message.reply(`${member.user.tag} has no warnings.`);
+        const warns = JSON.parse(fs.readFileSync(warnsPath, 'utf-8')) || {};
+        const userWarns = warns[message.guild.id]?.[member.id] || [];
+
+        if (userWarns.length === 0) {
+            return message.reply(`${member.user.tag} has no warnings.`);
         }
+
+        message.reply(`${member.user.tag} has the following warnings: ${userWarns.join(', ')}`);
     },
 };
