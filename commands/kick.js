b4e4f7c99cc631e42988dd44ebbc7875c0e5fa53
rustydev3 <157342958+rustydev3@users.noreply.github.com>
2024-09-21 21:08:56 +0530

    Update kick.js

--- commands/kick.js
+++ commands/kick.js
@@ -1,22 +1,15 @@
-const { PermissionsBitField } = require('discord.js');
-
 module.exports = {
     name: 'kick',
-    description: 'Kick a member from the server.',
-    async execute(message, args) {
-        if (!message.member.permissions.has(PermissionsBitField.Flags.KickMembers)) {
-            return message.reply("You don't have permission to kick members.");
-        }
-
+    description: 'Kick a member from the server',
+    run: async (client, message, args) => {
         const member = message.mentions.members.first();
         if (!member) return message.reply('Please mention a member to kick.');
 
-        try {
-            await member.kick();
-            message.reply(`Kicked ${member.user.tag} from the server.`);
-        } catch (err) {
-            console.error(err);
-            message.reply("I couldn't kick that member.");
+        if (!message.member.permissions.has('KICK_MEMBERS')) {
+            return message.reply("You don't have permission to use this command.");
         }
+
+        await member.kick();
+        message.reply(`${member.user.tag} has been kicked.`);
     },
 };
