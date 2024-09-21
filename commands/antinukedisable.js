7e253de91e5f30d341e7bfb2c427fe6d52a69db9
rustydev3 <157342958+rustydev3@users.noreply.github.com>
2024-09-21 21:08:31 +0530

    Update antinukedisable.js

--- commands/antinukedisable.js
+++ commands/antinukedisable.js
@@ -1,12 +1,17 @@
-const fs = require('fs');
-const config = require('../config.json');
+const { config } = require('../config.json');
 
 module.exports = {
     name: 'antinukedisable',
-    description: 'Disable anti-nuke features in this server.',
-    execute(message) {
-        config[message.guild.id] = { antinuke: false };
-        fs.writeFileSync('./config.json', JSON.stringify(config, null, 2));
-        message.reply('Anti-nuke features disabled.');
+    description: 'Disable anti-nuke features',
+    run: async (client, message, args) => {
+        if (!message.member.permissions.has('ADMINISTRATOR')) {
+            return message.reply("You don't have permission to use this command.");
+        }
+
+        config[message.guild.id] = config[message.guild.id] || {};
+        config[message.guild.id].antinuke = false;
+        message.reply('Anti-nuke has been disabled.');
+
+        // Optionally save the updated config to the file
     },
 };
