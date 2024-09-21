d108d107bf4ac97d955a6226cf6b27a5128f0885
rustydev3 <157342958+rustydev3@users.noreply.github.com>
2024-09-21 21:09:34 +0530

    Update mute.js

--- commands/mute.js
+++ commands/mute.js
@@ -1,25 +1,16 @@
-const { PermissionsBitField } = require('discord.js');
-
 module.exports = {
     name: 'mute',
-    description: 'Mute a member in the server.',
-    async execute(message, args) {
-        if (!message.member.permissions.has(PermissionsBitField.Flags.MuteMembers)) {
-            return message.reply("You don't have permission to mute members.");
-        }
-
+    description: 'Mute a member in the server',
+    run: async (client, message, args) => {
         const member = message.mentions.members.first();
         if (!member) return message.reply('Please mention a member to mute.');
 
         const muteRole = message.guild.roles.cache.find(role => role.name === 'Muted');
-        if (!muteRole) return message.reply('Mute role does not exist.');
-
-        try {
-            await member.roles.add(muteRole);
-            message.reply(`Muted ${member.user.tag}.`);
-        } catch (err) {
-            console.error(err);
-            message.reply("I couldn't mute that member.");
+        if (!muteRole) {
+            return message.reply('Mute role not found. Please create a role named "Muted".');
         }
+
+        await member.roles.add(muteRole);
+        message.reply(`${member.user.tag} has been muted.`);
     },
 };
